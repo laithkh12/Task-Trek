b3a3cec9e67d9b6e2c4da7d193a8bc42dec487aa
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-05-05 15:40:11 +0200

    Add files via upload

---
+++ Task-Trek/src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TaskForm from "./components/TaskFrom/TaskForm";
+import TaskColumn from "./components/TaskColumn/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
+
+const oldTasks = localStorage.getItem("tasks");
+
+const App = () => {
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+  const [activeCard, setActiveCard] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleDelete = (taskIndex) => {
+    const newTasks = tasks.filter((task, index) => index !== taskIndex);
+    setTasks(newTasks);
+  };
+
+  const onDrop = (status, position) => {
+    console.log(
+      `${activeCard} is going to place into ${status} and at the position ${position}`
+    );
+    if (activeCard === null || activeCard === undefined) return;
+
+    const taskToMove = tasks[activeCard];
+    const updatedTasks = tasks.filter((task, index) => index !== activeCard);
+
+    updatedTasks.splice(position, 0, {
+      ...taskToMove,
+      status: status,
+    });
+
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="app">
+      <TaskForm setTasks={setTasks} />
+      <main className="app_main">
+        <TaskColumn
+          tasks={tasks}
+          title="Todo"
+          icon={todoIcon}
+          status="todo"
+          handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
+        />
+        <TaskColumn
+          title="Doing"
+          tasks={tasks}
+          icon={doingIcon}
+          status="doing"
+          handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
+        />
+        <TaskColumn
+          title="Done"
+          icon={doneIcon}
+          tasks={tasks}
+          status="done"
+          handleDelete={handleDelete}
+          setActiveCard={setActiveCard}
+          onDrop={onDrop}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default App;
